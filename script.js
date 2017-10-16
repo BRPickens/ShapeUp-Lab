219e85b504fd078ddd1053cea06f9b1f29e43633
BRPickens <brpickens@gmail.com>
2017-10-16 10:23:54 -0500

    Some ShapeUp

--- script.js
+++ script.js
@@ -1,36 +1,124 @@
-var buttonContainer;
-var squareArray = [];
-var triangleArray = [];
-var rectangleArray = [];
-var circleArray = [];
-document.addEventListener('DOMContentLoaded', function () {
-    buttonContainer = document.getElementById('button-container');
-    //Created Buttons//
-    var SQUARE_INPUT = document.getElementById('input');
-    var SQUARE_BUTTON = document.getElementById('button');
-    var TRIANGLE_INPUT = document.getElementById('input');
-    var TRIANGLE_BUTTON = document.getElementById('button');
-    var RECTANGLE_WIDTH = document.getElementById('input');
-    var RECTANGLE_HEIGHT = document.getElementById('input');
-    var RECTANGLE_BUTTON = document.getElementById('button');
-    var CIRCLE_INPUT = document.getElementById('input');
-    var CIRCLE_BUTTON = document.getElementById('button');
-
-    class Shape {
-        
-    }
-
-    //Button Functions//
-    SQUARE_BUTTON.addEventListener('click', function () {
-        squareArray.push(new Square());
-    });
-    TRIANGLE_BUTTON.addEventListener('click', function () {
-        triangleArray.push(new Triangle());
-    });
-    RECTANGLE_BUTTON.addEventListener('click', function () {
-        rectangleArray.push(new Rectangle());
-    });
-    CIRCLE_BUTTON.addEventListener('click', function () {
-        circleArray.push(new Circle());
-    });
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+document.addEventListener("DOMContentLoaded", function () {
+    // * Each of the insert buttons above should create a new instance of that particular shape, which should draw itself on the screen with the specified size and placed in a random location (object details are below)
+    var RECTANGLE_BTN = document.getElementById('rectangle-btn');
+    var SQUARE_BTN = document.getElementById('square-btn');
+    var CIRCLE_BTN = document.getElementById('circle-btn');
+    var TRIANGLE_BTN = document.getElementById('triangle-btn');
+    RECTANGLE_BTN.addEventListener('click', shapeCreator('rectangle')); //another function that gathers the parameters
+    SQUARE_BTN.addEventListener('click', shapeCreator('square'));
+    CIRCLE_BTN.addEventListener('click', shapeCreator('circle'));
+    TRIANGLE_BTN.addEventListener('click', shapeCreator('triangle'));
+    //get each button by id and on click Each of the insert buttons above should create a new instance of that particular shape, which should draw itself on the screen with the specified size and placed in a random location (object details are below)
 });
+// * Rectangle: Inputs for width and height, and an insert button
+// * Square: Input for side length and an insert button
+// * Circle: Input for radius and an insert button
+// * Isoceles Right Triangle: Input for height and an insert button
+var Shape = /** @class */ (function () {
+    function Shape() {
+        var _this = this;
+        this.div = document.createElement('div');
+        this.draw();
+        this.div.className = 'shape';
+        this.div.addEventListener('dblclick', function () {
+            _this.div.remove();
+        });
+    }
+    Shape.prototype.draw = function () {
+        this.div.style.top = randomNumberOne() + "px"; //top should be between 1 to 475
+        this.div.style.left = randomNumberTwo() + "px"; //left should be between 453 to 916
+        document.getElementById('container').appendChild(this.div);
+    };
+    return Shape;
+}());
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle() {
+        var _this = _super.call(this) || this;
+        _this.div.id = 'rectangle';
+        _this.size((document.getElementById('r-width').value), (document.getElementById('r-height').value));
+        return _this;
+    }
+    Rectangle.prototype.size = function (width, height) {
+        this.div.style.width = width + "px";
+        this.div.style.height = height + "px";
+    };
+    return Rectangle;
+}(Shape));
+var Square = /** @class */ (function (_super) {
+    __extends(Square, _super);
+    function Square() {
+        var _this = _super.call(this) || this;
+        _this.div.id = 'square';
+        _this.size(document.getElementById('square-input').value);
+        return _this;
+    }
+    Square.prototype.size = function (sideLength) {
+        this.div.style.height = sideLength + "px";
+        this.div.style.width = sideLength + "px";
+    };
+    return Square;
+}(Shape));
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle() {
+        var _this = _super.call(this) || this;
+        _this.div.id = 'circle';
+        _this.size(document.getElementById('circle-input').value);
+        return _this;
+    }
+    Circle.prototype.size = function (size) {
+        this.div.style.height = size + "px";
+        this.div.style.width = size + "px";
+        this.div.style.borderRadius = size / 2 + "px";
+    };
+    return Circle;
+}(Shape));
+var Triangle = /** @class */ (function (_super) {
+    __extends(Triangle, _super);
+    function Triangle() {
+        var _this = _super.call(this) || this;
+        _this.div.id = 'triangle-right';
+        _this.size(document.getElementById('triangle-input').value);
+        return _this;
+    }
+    Triangle.prototype.size = function (size) {
+        this.div.style.borderBottom = size + "px solid #f1c40f ";
+        this.div.style.borderRight = size + "px solid transparent";
+    };
+    return Triangle;
+}(Shape));
+//Functions
+var shapeCreator = function (type) {
+    return function () {
+        switch (type) {
+            case 'rectangle':
+                new Rectangle();
+                break;
+            case 'square':
+                new Square();
+                break;
+            case 'circle':
+                new Circle();
+                break;
+            case 'triangle':
+                new Triangle();
+        }
+    };
+};
+var randomNumberOne = function () {
+    return Math.floor((Math.random() * 475) + 1);
+};
+var randomNumberTwo = function () {
+    return Math.floor((Math.random() * 916) + 453);
+};
--- script.ts
+++ script.ts
@@ -1,39 +1,118 @@
-let squareLength,
-    circleRadius,
-    triangleHeight,
-    rectangleHeight,
-    rectangleWidth,
-    field;
 
-document.addEventListener('DOMContentLoaded', () => {
-    buttonContainer = document.getElementById('button-container');
-//Created Buttons//
-    const squareLength: HTMLElement = document.getElementById('input');
-    const SQUARE_BUTTON: HTMLElement = document.getElementById('button');
-    const TRIANGLE_INPUT: HTMLElement = document.getElementById('input');
-    const TRIANGLE_BUTTON: HTMLElement = document.getElementById('button');
-    const RECTANGLE_WIDTH: HTMLElement = document.getElementById('input');
-    const RECTANGLE_HEIGHT: HTMLElement = document.getElementById('input');
-    const RECTANGLE_BUTTON: HTMLElement = document.getElementById('button');
-    const CIRCLE_INPUT: HTMLElement = document.getElementById('input');
-    const CIRCLE_BUTTON: HTMLElement = document.getElementById('button');
+document.addEventListener("DOMContentLoaded", function( ) {
 
-// Constructor Bullshit 
+const RECTANGLE_BTN = document.getElementById('rectangle-btn');
+const SQUARE_BTN = document.getElementById('square-btn');
+const CIRCLE_BTN = document.getElementById('circle-btn');
+const TRIANGLE_BTN = document.getElementById('triangle-btn');
 
-//Button Functions//
-    SQUARE_BUTTON.addEventListener('click',() => {
-        squareArray.push( new Square());
-    });
-
-    TRIANGLE_BUTTON.addEventListener('click', () => {
-        triangleArray.push( new Triangle());
-    });
-
-    RECTANGLE_BUTTON.addEventListener('click', () => {
-        rectangleArray.push( new Rectangle());
-    });
-
-    CIRCLE_BUTTON.addEventListener('click', () => {
-        circleArray.push( new Circle());
-    });
+RECTANGLE_BTN.addEventListener('click', shapeCreator('rectangle'));
+SQUARE_BTN.addEventListener('click', shapeCreator('square'));
+CIRCLE_BTN.addEventListener('click', shapeCreator('circle'));
+TRIANGLE_BTN.addEventListener('click',shapeCreator('triangle'))
 });
+
+class Shape {
+    div: HTMLDivElement = document.createElement('div');
+    type: string;
+
+constructor(){
+    this.draw();
+    this.div.className = 'shape';  
+    this.div.addEventListener('dblclick', () => {
+        this.div.remove(); 
+    });
+    
+}
+draw(): void {
+    this.div.style.top = `${randomNumberOne()}px`;
+    this.div.style.left = `${randomNumberTwo()}px`;
+    document.getElementById('container').appendChild(this.div); 
+} 
+
+}
+
+class Rectangle extends Shape {
+
+
+constructor(){
+    super()
+    this.div.id ='rectangle';
+    
+    this.size(((<HTMLInputElement>document.getElementById('r-width')).value), ((<HTMLInputElement>document.getElementById('r-height')).value)) ; 
+}
+size(width, height): void {
+    this.div.style.width = `${width}px`;
+    this.div.style.height = `${height}px`;
+}
+}
+
+class Square extends Shape {
+
+constructor(){
+    super()
+    this.div.id ='square';
+    this.size((<HTMLInputElement>document.getElementById('square-input')).value);
+}
+size(sideLength): void {
+    this.div.style.height = `${sideLength}px`;
+    this.div.style.width = `${sideLength}px`; 
+}
+}    
+
+class Circle extends Shape {
+
+constructor(){
+    super()
+    this.div.id ='circle';
+    this.size((<HTMLInputElement>document.getElementById('circle-input')).value);  
+}
+size(size): void {
+    this.div.style.height = `${size}px`;
+    this.div.style.width = `${size}px`;
+    this.div.style.borderRadius = `${size/2}px`;
+}
+}
+
+class Triangle extends Shape {
+
+constructor(){
+    super()
+    this.div.id ='triangle-right'; 
+    this.size((<HTMLInputElement>document.getElementById('triangle-input')).value)
+}
+size(size): void {
+    this.div.style.borderBottom = `${size}px solid #f1c40f `;
+    this.div.style.borderRight = `${size}px solid transparent`
+}
+}
+
+//Functions
+
+let shapeCreator = (type)=>{
+return () => {
+    switch(type) {
+        case 'rectangle':
+            new Rectangle();
+            
+            
+            break;
+        case 'square':
+            new Square();
+            break;
+        case 'circle':
+            new Circle();
+            break;
+        case 'triangle':
+            new Triangle();
+    }
+}
+}
+
+let randomNumberOne = (): any => {
+return Math.floor((Math.random() * 475) + 1);
+}
+
+let randomNumberTwo = (): any => {
+return Math.floor((Math.random() * 916) + 453);
+}
